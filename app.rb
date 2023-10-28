8e31e9ff7797414add88325eef7fd0ea3e5d16fd
ashih92 <146589396+ashih92@users.noreply.github.com>
2023-10-28 23:48:51 +0000

    commit

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
@@ -17,13 +17,13 @@ get("/wizard_add") do
 end
 
 get("/subtract") do
-  erb(:sub_form)
+  erb(:subtraction_form)
 end
 
 get("/wizard_subtract") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
-
+  @result = @first_num - @second_num
   erb(:sub_result)
 end
 
